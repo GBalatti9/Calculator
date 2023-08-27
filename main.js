13c4e7fc0ad0f5a6869d674872a7f41b0fd5ea81
GBalatti9 <gas.balatti@gmail.com>
2023-08-27 10:00:16 -0300

    White the JS file i've created the grid buttons of the calculator and also in the same function i 've asigned the value for each button

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 window.onload = () => {
-    console.log('prueba')
+    console.log('prueba');
+
+    // CATCH DOM ELEMENTS
+    const operationContainer = document.querySelector('.calculator-buttons-numbers');
+
+    // FUNCTIONS
+    const buttonValues = [
+        '7', '8', '9', '/',
+        '4', '5', '6', 'x',
+        '1', '2', '3', '-',
+        '.', '0', '=', '+'
+    ];
+    const displayElements = (num, content) => {
+        for (let i = 0; i < num; i++) {
+            const div = document.createElement('div');
+            div.classList.add('div-number');
+            div.textContent = content[i]
+            operationContainer.appendChild(div);
+        }
+    }
+
+    displayElements(buttonValues.length, buttonValues)
 
     
 }
