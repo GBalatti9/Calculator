b2f06bf23e6958ca3453aeb0faa33c4d2cbebbfc
GBalatti9 <gas.balatti@gmail.com>
2023-08-27 11:27:58 -0300

    Display numbers The numbers and their behaviour works ok: delete and clear buttons. Just display interactions, not functions

--- main.js
+++ main.js
@@ -19,8 +19,95 @@ window.onload = () => {
             operationContainer.appendChild(div);
         }
     }
-
     displayElements(buttonValues.length, buttonValues)
 
-    
+    // CATCH BUTTONS
+    const clearBtn = document.querySelector('.clear-btn');
+    const deleteBtn = document.querySelector('.delete-btn');
+    const calculatorBtns = [...document.querySelectorAll('.div-number')];
+    const numberDisplay = document.querySelector('.init');
+    const operationDisplay = document.querySelector('.operation');
+    operationDisplay.textContent = ''
+
+    let firstNumber = '';
+    let operator = '';
+    let secondNumber = '';
+    let currentExpression = '';
+
+    calculatorBtns.forEach(btn => {
+        btn.addEventListener('click', e => {
+            let btnValue = e.target.textContent;
+            if (/^(\d+(\.\d*)?)$/.test(btnValue)) {
+                if (operator === '') {
+                    if (firstNumber === 0) {
+                        firstNumber = btnValue;
+                    } else {
+                        firstNumber += btnValue;
+                    }
+                    operationDisplay.textContent = firstNumber;
+                    currentExpression = firstNumber;
+                    numberDisplay.textContent = firstNumber;
+                } else {
+                    if (secondNumber === 0) {
+                        secondNumber = btnValue;
+                    } else {
+                        secondNumber += btnValue;
+                    }
+                    currentExpression = `${firstNumber} ${operator} ${secondNumber}`;
+                    operationDisplay.textContent = currentExpression;
+                    numberDisplay.textContent = secondNumber;
+                }
+            } else {
+                operator = btnValue;
+                if (currentExpression !== '') {
+                    currentExpression = `${firstNumber} ${operator}`;
+                }
+                operationDisplay.textContent = currentExpression;
+            }
+        })
+    })
+
+    clearBtn.addEventListener('click', () => {
+        firstNumber = '';
+        secondNumber = '';
+        operator = '';
+        currentExpression = '';
+        numberDisplay.textContent = 0
+        operationDisplay.textContent = ''
+    })
+
+    deleteBtn.addEventListener('click', () => {
+        if(operator === ''){
+
+            let opD = operationDisplay.textContent.split('')
+            opD.pop();
+            let newOpD = opD.join('');
+            operationDisplay.textContent = newOpD;
+
+            let arr = firstNumber.split('');
+            arr.pop();
+            let newNum = arr.join('');
+            firstNumber = newNum
+            numberDisplay.textContent = newNum;
+        } else {
+            let opD = operationDisplay.textContent.split('')
+            opD.pop();
+            let newOpD = opD.join('');
+            operationDisplay.textContent = newOpD;
+            
+            let arr = secondNumber.split('');
+            arr.pop()
+            let newNum = arr.join('');
+            secondNumber = newNum;
+            numberDisplay.textContent = newNum;
+        }
+    })
+
+    const add = (a, b) => a + b;
+    const substract = (a, b) => a - b;
+    const multiply = (a, b) => a * b;
+    const divide = (a, b) => a / b;
+
+
+
 }
